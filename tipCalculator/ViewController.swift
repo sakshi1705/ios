921f0333b37c2a6aa58a564ff5226fe38e610ae1
sakshi tyagi <sakshtyagi@gmail.com>
2019-01-17 20:40:03 -0800

    ios tipcalculator

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by sweta kumari on 12/26/18.
+//  Copyright © 2018 sweta kumari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPecentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPecentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+}
+
